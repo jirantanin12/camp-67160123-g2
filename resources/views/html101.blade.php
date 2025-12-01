8a7ae83c49edab84d3580239c3eadb8eba54d35c
jirantanin12 <jirantanin1812@gmail.com>
2025-12-01 17:22:40 +0700

    งานชิ้นที่ 1

---
+++ resources/views/html101.blade.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset = "utf-8"/>
+       <title> Workshop HTML</title>
+       <link rel="stylesheet" href="css/bootstrap.css"/>
+    </head>
+    <body>
+          <div class = "container pt-5">
+                     <h1>Workshop #HTML - FORM </h1>
+                   <div class="mb-3 row">
+                         <label for="inputFirstName" class="col-sm-2 col-form-label">ชื่อ</label>
+                      <div class="col-sm-10">
+                          <input type="text"   class="form-control" id="inputFirstname" placeholder= "Firstname">
+                      </div>
+                   </div>
+
+                   <div class="mb-3 row">
+                         <label for="inputLastName" class="col-sm-2 col-form-label">นามสกุล</label>
+                   <div class="col-sm-10">
+                         <input type="text" class="form-control" id="inputLastname"  placeholder= "Lastname">
+                     </div>
+                   </div>
+
+                 <div class="mb-3 row">
+                         <label for="inputPassword" class="col-sm-2 col-form-label">วัน/เดือน/ปี</label>
+                   <div class="col-sm-10">
+                         <input type="date" class="form-control" id="bdate">
+                   </div>
+                 </div>
+         </div>
+</body>
+</html>
